9909ad99a1ae521fbae3b6a821956cf518d5cd82
sahilsinghsimform <sahilsinghsimform@gmail.com>
2022-12-05 12:21:58 +0530

    updating files

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,20 +1,20 @@
 import nc from "next-connect";
-// import Product from "../../src/models/Product";
-// import db from "../../src/utils/db";
-// import data from "../../src/utils/data";
-// import User from "../../src/models/User";
+import Product from "../../src/models/Product";
+import db from "../../src/utils/db";
+import data from "../../src/utils/data";
+import User from "../../src/models/User";
 
 const handler = nc();
 
 handler.get(async (req, res) => {
-  return res.send({ message: "already seeded" });
-  // await db.connect();
-  // await User.deleteMany();
-  // await User.insertMany(data.users);
-  // await Product.deleteMany();
-  // await Product.insertMany(data.products);
-  // await db.disconnect();
-  // res.send({ message: "seeded successfully" });
+  // return res.send({ message: "already seeded" });
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
+  await db.disconnect();
+  res.send({ message: "seeded successfully" });
 });
 
 export default handler;
